7c42f2a5bc253daa66713e949a5613a64c633d4e
unknown <vuuthongnguyen@gmail.com>
2022-09-22 20:19:16 +0700

    cs50:django- #t=47m21s

---
+++ hello/views.py
@@ -0,0 +1,13 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello,world!")
+def mike(request):
+    return HttpResponse("hello Mike Vuu")
+
+def greet(request,name):
+    return render(request, "hello/greet.html",{
+        "name": name.capitalize()
+    })
